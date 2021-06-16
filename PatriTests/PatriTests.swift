fc13c88cd035f444999f00315478e0a0dea925e7
Avnish Kumar <avnish015@gmail.com>
2021-06-17 00:55:09 +0530

    Added Test Cases

---
+++ PatriTests/PatriTests.swift
@@ -0,0 +1,34 @@
+//
+//  PatriTests.swift
+//  PatriTests
+//
+//  Created by Avnish on 17/06/21.
+//
+
+import XCTest
+
+class PatriTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let i = 10
+        XCTAssert(i == 10  , "Test shold fail")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
